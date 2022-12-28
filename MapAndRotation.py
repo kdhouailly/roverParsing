9bdc5b1b440cbcafe29b5c01db1ecdc3d82cd209
LeMagnifique <arnaud.dellam@gmail.com>
2022-12-28 15:58:06 +0100

    correction bug kao v2

---
+++ MapAndRotation.py
@@ -0,0 +1,28 @@
+from enum import Enum
+
+class Rotation(Enum):
+    N = 0
+    E = 1
+    S = 2
+    W = 3
+    def GetRotation(actualrotation,sens):
+        if actualrotation == Rotation.N and sens == -1:
+            return Rotation.W
+        elif actualrotation ==  Rotation.W and sens == 1:
+            return Rotation.N
+        else:
+            return Rotation(actualrotation.value + sens)
+
+class Map:
+    def __init__(self,filename) -> None:
+        self.matriceMap = []
+        file = open(filename,"r")
+        code = file.readlines()
+        file.close()
+        for i in code:
+            self.matriceMap.append(list(i.strip()))
+    def printMap(self):
+        #os.system("clear")
+        print()
+        for i in self.matriceMap:
+            print("".join(i))
